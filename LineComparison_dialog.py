8084ce949f5bc518f512a77425d720f1d0ce07f3
Sophie Crommelinck <sophie.crommelinck@gmail.com>
2019-09-24 10:30:29 +0200

    Update LineComparison_dialog.py

--- LineComparison_dialog.py
+++ LineComparison_dialog.py
@@ -1,33 +1,43 @@
-# -*- coding: utf-8 -*-
 """
-/***************************************************************************
- LineComparisonDialog
-                                 A QGIS plugin
-                             -------------------
+!/bin/python
+-*- coding: utf-8 -*-
+/*****************************************************************************
         begin                : 2018-08-01
-        git sha              : $Format:%H$
-        copyright            : (C) 2018 by Sophie Crommelinck
+        copyright            : (C) 2019 by Sophie Crommelinck, 
+                                University of Twente
         email                : s.crommelinck@utwente.nl
-        icon                 : Line Graph by Calvin Goodman from the Noun Project
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+        description          : this script takes probabilities of image tiles
+                                averages them based on its ID value and writes
+                                this value to the corresponding line feature.
+        funding              : H2020 EU project its4land 
+                                (#687826, its4land.com)
+                                Work package 5: Automate It
+        development          : Sophie Crommelinck
+        icon                 : Line Graph by Calvin Goodman from Noun Project
+ *****************************************************************************/
+ 
+ /*****************************************************************************
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.  *
+  *****************************************************************************/
 """
 
+# Import modules
 import os
-
 from PyQt5 import uic
 from PyQt5.QtCore import QSettings, QTranslator, qVersion, Qt
 from PyQt5.QtGui import QIcon, QColor, QPixmap
 from PyQt5.QtWidgets import QDialog, QAction, QFileDialog, QToolBar
-
 from .ComparisonController import *
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
